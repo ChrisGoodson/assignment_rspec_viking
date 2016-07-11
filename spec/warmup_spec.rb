4a053fa668572a2a7176013d7944e88e606346dc
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-11 13:10:15 -0500

    working on calls some methods

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -19,5 +19,21 @@ describe Warmup do
       expect(warmup.triple_size(my_array)).to eq(12)
     end
   end
+
+  describe "#calls_some_methods" do
+    # let(:my_string) {double("string", :upcase! => "UNBELIEVABLE")}
+    it "returns a new string after receiving a bang" do
+      my_string = "unbelievable"
+      expect(warmup.calls_some_methods(my_string)).to receive(:upcase!).and_return("UNBELIEVABLE")
+      # calls_some_methods(my_string)
+
+    end
+    it "invokes the destructive reverse method" do
+      expect(warmup).to receive(:reverse!).and_return("toag")
+      calls_some_methods("goat")
+    end
+  end
+
+
 end
 
